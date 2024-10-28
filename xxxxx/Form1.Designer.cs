1c436585040d80f446150f8f3662770065dc33ff
saadhzw <saadhzw@gmail.com>
2024-10-28 13:42:22 +0300

    Add project files.

---
+++ xxxxx/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace xxxxx
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_ResultLs = new System.Windows.Forms.TextBox();
+            this.btn_Calc = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_Wq = new System.Windows.Forms.TextBox();
+            this.txt_Ws = new System.Windows.Forms.TextBox();
+            this.txt_Result = new System.Windows.Forms.TextBox();
+            this.txt_Lambda = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1868, 1055);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.txt_ResultLs);
+            this.tabPage1.Controls.Add(this.btn_Calc);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.txt_Wq);
+            this.tabPage1.Controls.Add(this.txt_Ws);
+            this.tabPage1.Controls.Add(this.txt_Result);
+            this.tabPage1.Controls.Add(this.txt_Lambda);
+            this.tabPage1.Location = new System.Drawing.Point(4, 37);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.tabPage1.Size = new System.Drawing.Size(1860, 1014);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(763, 791);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 28);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Ls";
+            // 
+            // txt_ResultLs
+            // 
+            this.txt_ResultLs.Location = new System.Drawing.Point(386, 779);
+            this.txt_ResultLs.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.txt_ResultLs.Name = "txt_ResultLs";
+            this.txt_ResultLs.Size = new System.Drawing.Size(336, 35);
+            this.txt_ResultLs.TabIndex = 9;
+            // 
+            // btn_Calc
+            // 
+            this.btn_Calc.Location = new System.Drawing.Point(386, 474);
+            this.btn_Calc.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.btn_Calc.Name = "btn_Calc";
+            this.btn_Calc.Size = new System.Drawing.Size(340, 56);
+            this.btn_Calc.TabIndex = 8;
+            this.btn_Calc.Text = "Calc";
+            this.btn_Calc.UseVisualStyleBackColor = true;
+            this.btn_Calc.Click += new System.EventHandler(this.btn_Calc_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(763, 654);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 28);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Lq";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(786, 338);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 28);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Ws";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(786, 203);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 28);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Wq";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(786, 96);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 28);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "lambda";
+            // 
+            // txt_Wq
+            // 
+            this.txt_Wq.Location = new System.Drawing.Point(386, 191);
+            this.txt_Wq.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.txt_Wq.Name = "txt_Wq";
+            this.txt_Wq.Size = new System.Drawing.Size(336, 35);
+            this.txt_Wq.TabIndex = 3;
+            this.txt_Wq.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.AcceptOnlyNumberInTextBox_KeyPress);
+            // 
+            // txt_Ws
+            // 
+            this.txt_Ws.Location = new System.Drawing.Point(386, 326);
+            this.txt_Ws.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.txt_Ws.Name = "txt_Ws";
+            this.txt_Ws.Size = new System.Drawing.Size(336, 35);
+            this.txt_Ws.TabIndex = 2;
+            this.txt_Ws.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.AcceptOnlyNumberInTextBox_KeyPress);
+            // 
+            // txt_Result
+            // 
+            this.txt_Result.Location = new System.Drawing.Point(386, 642);
+            this.txt_Result.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.txt_Result.Name = "txt_Result";
+            this.txt_Result.Size = new System.Drawing.Size(336, 35);
+            this.txt_Result.TabIndex = 1;
+            // 
+            // txt_Lambda
+            // 
+            this.txt_Lambda.Location = new System.Drawing.Point(386, 84);
+            this.txt_Lambda.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.txt_Lambda.Name = "txt_Lambda";
+            this.txt_Lambda.Size = new System.Drawing.Size(336, 35);
+            this.txt_Lambda.TabIndex = 0;
+            this.txt_Lambda.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.AcceptOnlyNumberInTextBox_KeyPress);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 37);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.tabPage2.Size = new System.Drawing.Size(1860, 1014);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 37);
+            this.tabPage3.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.tabPage3.Size = new System.Drawing.Size(1860, 1014);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1868, 1055);
+            this.Controls.Add(this.tabControl1);
+            this.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_Wq;
+        private System.Windows.Forms.TextBox txt_Ws;
+        private System.Windows.Forms.TextBox txt_Result;
+        private System.Windows.Forms.TextBox txt_Lambda;
+        private System.Windows.Forms.Button btn_Calc;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_ResultLs;
+    }
+}
+
---
+++ xxxxx/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace xxxxx
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //LS
+            txt_ResultLs.ReadOnly = true;
+            //LQ
+            txt_Result.ReadOnly = true;
+        }
+        private void AcceptOnlyNumberInTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar)
+                            && !char.IsDigit(e.KeyChar)
+                            && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+        private void btn_Calc_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // قراءة المدخلات
+                double lambda = Convert.ToDouble(txt_Lambda.Text);
+                double wq = Convert.ToDouble(txt_Wq.Text);
+                double ws = Convert.ToDouble(txt_Ws.Text);
+                // حساب Lq و Ls
+                double Lq = lambda * wq;
+                double Ls = lambda * ws;
+                txt_ResultLs.Text = Ls.ToString();
+                txt_Result.Text = Lq.ToString();
+            }
+            catch (Exception ex)
+            {
+                if (ex is FormatException)
+                {
+                    MessageBox.Show("يرجى التأكد من إدخال قيم صحيحة.");
+
+                }
+                else
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+    }
+}
